e582ecd87db9b027e8d3c2473e21e98f74e60f0f
Juhee-Park <ttaro2ho@gmail.com>
2023-02-25 20:45:11 +0900

    wert

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ local composer = require "composer"
 
 -- event listeners for tab buttons:
 local function onFirstView( event )
-	composer.gotoScene( "start" )
+	composer.gotoScene( "clear" )
 end
 
 
