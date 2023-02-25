ccb8183ed89760a0ac356959444aee379872151a
jisoo <wljitnlim99@gmail.com>
2023-02-25 13:19:32 +0900

    점수 및 클리어 화면 출력

--- ending.lua
+++ ending.lua
@@ -1,63 +1,53 @@
 -----------------------------------------------------------------------------------------
 --
--- view1.lua
+-- ending.lua
 --
 -----------------------------------------------------------------------------------------
 
 local composer = require( "composer" )
 local scene = composer.newScene()
 
+
 function scene:create( event )
 	local sceneGroup = self.view
-	--main화면
-	local background = display.newImageRect("image/main_background.png", display.contentWidth, display.contentHeight)
+    --클리어 화면 출력
+	local background = display.newImageRect("image/clear_background.png", display.contentWidth, display.contentHeight)
 	background.x, background.y = display.contentWidth/2, display.contentHeight/2
+    --게임종료
+	local out = display.newImageRect("image/clear/out.png", 1000, 200)
+	out.x, out.y = display.contentWidth*0.5, display.contentHeight*0.9
+    --다시하기 
+	local retry = display.newImageRect("iamge/clear/retry.png",1000 , 200)
+	retry.x, retry.y = display.contentWidth*0.5, display.contentHeight*0.8
+    --클리어
+	local clear = display.newImageRect("image/clear/clear.png", 1000, 200)
+	clear.x, clear.y = display.contentWidth*0.5, display.contentHeight*0.15
 
-	--제조성공시 점수
-	local score = display.newText(0, 660, 660, display.contentWidth*0.1, display.contentHeight*0.15) --성공시 점수
-	score.size = 100
-	score:setFillColor(0)
-	--score.alpha = 0.5
-	--성공주문수
-	local ordern = display.newImageRect("image/main/sucess_count.png", 500, 150)
-	ordern.x, ordern.y = display.contentWidth*0.22, display.contentHeight*0.21
-
-	--제조 실패시 점수
-	local scoref = display.newText(0, 1600, 660, display.contentWidth*0.5, display.contentHeight*0.15) --실패시 점수
-	scoref.size = 100
-	scoref:setFillColor(0)
-	--scoref.alpha = 0.5
-	--실패 주문 수
-	local orderm = display.newImageRect("image/main/fail_account.png", 500, 150)
-	orderm.x, orderm.y = display.contentWidth*0.66, display.contentHeight*0.21
-	
-	
 	sceneGroup:insert(background)
-	sceneGroup:insert(ordern)
-	sceneGroup:insert(orderm)
-	sceneGroup:insert(score)
-	sceneGroup:insert(scoref)
-	
-	if  true then --제조 성공시 
-		score.text = score.text+1
-		if (score.text == '20') then
-		--score.text = 'clear!'
-		--time.alpha = 0
-		composer.gotoScene('ending')
-		end
-	else 	--제조 실패시
-		scoref.text = scoref.text+1
-		if (scoref.text == '5') then
-		composer.gotoScene('endingf')
-		end
+	sceneGroup:insert(out)
+	sceneGroup:insert(retry)
+	sceneGroup:insert(clear)
+
+	function retry:tap( event )
+ 		--
+ 	end
+
+ 	retry:addEventListener("tap", retry) -- 다시하기 누르면 게임화면으로
+
+	sceneGroup:insert(out)
+	sceneGroup:insert(retry)
+
+	function retry:tap( event )
+		composer.gotoScene('view1')
 	end
 
-
 end
+
+
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
+
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
@@ -65,26 +55,30 @@ function scene:show( event )
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
+
 	end	
 end
 
 function scene:hide( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
+
 	if event.phase == "will" then
 		-- Called when the scene is on screen and is about to move off screen
 		--
 		-- INSERT code here to pause the scene
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
+
+
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
+
 	end
 end
 
 function scene:destroy( event )
 	local sceneGroup = self.view
-	
+
 	-- Called prior to the removal of scene's "view" (sceneGroup)
 	-- 
 	-- INSERT code here to cleanup the scene
